8134cad6ae632c530c9af36a4363d0d1a650b6c0
Florian Bosshard <florian.bosshard@gmail.com>
2017-05-07 17:03:42 +0200

    add router

---
+++ src/app/participant-detail/participant-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'participant-detail',
+  templateUrl: './participant-detail.component.html',
+  styleUrls: ['./participant-detail.component.css']
+})
+export class ParticipantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
