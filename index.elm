eff7666c9f912e42445ad6a8a90991060f61050c
Florian Bosshard <florian.bosshard@gmail.com>
2017-09-13 21:12:52 +0200

    show list of participants

--- index.elm
+++ index.elm
@@ -5,7 +5,10 @@ import Html exposing (beginnerProgram, div, button, text, ul, li)
 
 
 main =
-  beginnerProgram { model = person, view = view, update = update }
+  beginnerProgram { model = tnList, view = view, update = update }
+
+
+type alias Tns = { personen: List}
 
 
 type alias Person =
@@ -14,24 +17,36 @@ type alias Person =
   }
 
 
-person : Person
-person =
+luxus : Person
+luxus =
   Person "Roman" "Luxus"
 
-view person =
-  ul []
-    [ 
-     li [] [ text (person.scoutname) ]
+prusik : Person
+prusik =
+  Person "Florian" "Prusik"
+
+tnList : List Person
+tnList = [luxus, prusik]
+
+view : List Person -> Html.Html Msg
+view tnList =
+  ul []   (List.map personRow tnList)
+  
+personRow: Person -> Html.Html Msg
+personRow person =
+ li [] [ 
+    text(person.scoutname)
     ]
+
 
 
 type Msg = ViewDetail
 
 
-update msg person = 
+update msg luxus = 
     case msg of 
     ViewDetail ->
-        person
+        luxus
 --  case msg of
 --    Increment ->
 --      person + 1
