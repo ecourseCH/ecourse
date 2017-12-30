a3a53a897e65e16c2706e56f7a84a63b890ede5e
Florian Bosshard <florian.bosshard@gmail.com>
2017-12-30 21:35:37 +0100

    adding http request, adding refresh every 30 seconds

---
+++ Data.elm
@@ -0,0 +1,27 @@
+module Data exposing (..)
+import Http
+import Json.Decode as Decode
+
+type alias Participant =
+    { name : String
+    , prename : String
+    , scoutName : String
+    }
+
+-- url = "http://prusik.ch/ecourse/participants.json"
+url : String
+url = "http://localhost:5000/api/participant"
+
+getParticipants : Http.Request (List Participant)
+getParticipants =
+  Http.get url (Decode.list decodeParticipant)
+
+decodeParticipant : Decode.Decoder Participant
+decodeParticipant =
+  Decode.map3 Participant
+    (Decode.field "name" Decode.string)
+    (Decode.field "preName" Decode.string)
+    (Decode.field "scoutName" Decode.string)
+
+
+
---
+++ Main.elm
@@ -0,0 +1,81 @@
+module Main exposing (..)
+ 
+import Html exposing (..)
+import Html.Attributes as HA exposing (..)
+import Html.Events as HE exposing (..)
+import Data exposing (..)
+import Http
+import Time exposing (Time, second)
+
+
+type alias Model =
+    { participants : List Participant
+    }
+
+
+type Msg
+    = OnClick
+    | Tick Time
+    | ParticipantsLoaded (Result Http.Error (List Participant))
+  
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { participants = [] }, sendRequest )
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [][
+         showList model
+        ]
+
+
+showList : Model -> Html Msg
+showList model =
+    table [] (List.map viewParticipant model.participants)
+
+
+viewParticipant : Participant -> Html Msg
+viewParticipant part =
+    tr [] [
+      td [] 
+      [ text <| part.prename ++ " " ++ part.name ++ " v/o " ++ part.scoutName ]
+    ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        OnClick ->
+            (model, sendRequest)
+        Tick newTime ->
+          (model, sendRequest)
+        ParticipantsLoaded (Ok participants) ->
+            ( { participants = participants }, Cmd.none )
+
+        ParticipantsLoaded (Err err) ->
+            let xx = Debug.log "oh, noes" (toString err)
+            in (model, Cmd.none)
+            
+            
+
+sendRequest : Cmd Msg
+sendRequest =
+  Http.send ParticipantsLoaded Data.getParticipants
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 30000 Tick
